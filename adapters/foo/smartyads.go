cfc7fefd04e4e04d01c74855817585d1c91baef8
Onkar Hanumante <onkarvhanumante@gmail.com>
2023-08-04 12:28:06 +0530

    t

--- adapters/foo/smartyads.go
+++ adapters/foo/smartyads.go
@@ -15,6 +15,16 @@ import (
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
+// test
+// test
+// test
+// test
+// test
+// test
+// test
+// test
+// test
+// test
 type SmartyAdsAdapter struct {
 	endpoint *template.Template
 }
